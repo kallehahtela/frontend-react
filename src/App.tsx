49a1e567156b6aa39d32b00f0b43001411388871
kallehahtela <sk1mb4yt@gmail.com>
2025-01-25 15:27:23 +0200

    Search bar mobile view with tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+import SearchBar from './components/SearchBar'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex'>
+        <SearchBar />
+      </div>
+    </>
+  )
+}
+
+export default App
