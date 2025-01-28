3114656f0a6ace232e60fa847742f24ec88141c6
kallehahtela <sk1mb4yt@gmail.com>
2025-01-28 17:44:36 +0200

    Added a Text information box which which can be used in a multiple places all over again

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Carousel from './components/Carousel'
 import Hero from './components/Hero'
 import InfiniteScroll from './components/InfiniteScroll'
 import Services from './components/Services'
+import TextBoxDark from './ui/TextBoxDark'
 
 
 
@@ -16,11 +17,15 @@ function App() {
         className='flex flex-col items-center overflow-x-hidden overflow-y-auto min-h-screen bg-gray-100'>
         <SearchBar placeholder={`Don't be lazy, get productive...`}/>
         <Carousel />
-        <Hero 
+        <Hero
           title={`Let's Make <br /> Difference on<br /> Your Web <br /> Presence Today`} 
         />
         <InfiniteScroll />
         <Services />
+        <TextBoxDark 
+            title='Choose us for your web journey!' 
+            text={`We provide the most developer <br /> like environment for non-code web <br /> development. Now you have a great <br /> change to get evolved in the change <br /> of website and web application <br /> development. There is none previous <br /> experience in coding & design <br /> required in order to make beautiful <br /> and well structured websites or web <br /> applications. There is an easy to <br /> follow along documentation to get <br /> started. Also there is some tutorials <br /> for how to add a certain feature in.`}
+          />
       </div>
     </>
   )
---
+++ src/ui/TextBoxDark.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "./Button";
+
+type TextProps = {
+    title: string;
+    text: string;
+    className?: string;
+};
+
+const TextBoxDark:React.FC<TextProps> = ({ title, text, className }) => {
+    const textParts = text.split('<br />');
+
+    return (
+        <div 
+            className={`relative bg-[#171717] flex flex-col mb-10 w-3/4 text-center rounded-md py-8${className || ''}`}
+        >
+            
+             {title && <p className="mt-2 font-bold text-2xl text-left text-white ml-10">{title}</p>}
+
+             <h4 className="text-sm text-left text-white ml-10 mt-2">
+                {textParts.map((part, index) => (
+                    <span key={index}>
+                        {part}
+                        {index < textParts.length -1 && <br />}
+                    </span>
+                ))}
+            </h4>
+        <Button 
+            title="Pricing"
+            className="relative w-2/4 ml-20 mt-5 text-lg font-semibold"
+        />
+        </div>
+    );
+}
+
+export default TextBoxDark;
